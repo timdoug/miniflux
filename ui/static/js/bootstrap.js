7208925b116c1b3eeed89394d985f943935fa9eb
Tim Douglas <me@timdoug.com>
2019-08-04 21:25:05 +0000

    Fix key bindings

--- ui/static/js/bootstrap.js
+++ ui/static/js/bootstrap.js
@@ -23,12 +23,12 @@ document.addEventListener("DOMContentLoaded", function () {
         keyboardHandler.on("V", () => openOriginalLink(true));
         keyboardHandler.on("c", () => openCommentLink());
         keyboardHandler.on("C", () => openCommentLink(true));
-        keyboardHandler.on("m", () => handleEntryStatus("next"));
-        keyboardHandler.on("M", () => handleEntryStatus("previous"));
+        keyboardHandler.on("u", () => handleEntryStatus("next"));
+        keyboardHandler.on("U", () => handleEntryStatus("previous"));
         keyboardHandler.on("A", () => markPageAsRead());
-        keyboardHandler.on("s", () => handleSaveEntry());
+        keyboardHandler.on("S", () => handleSaveEntry());
         keyboardHandler.on("d", () => handleFetchOriginalContent());
-        keyboardHandler.on("f", () => handleBookmark());
+        keyboardHandler.on("s", () => handleBookmark());
         keyboardHandler.on("F", () => goToFeed());
         keyboardHandler.on("R", () => handleRefreshAllFeeds());
         keyboardHandler.on("?", () => showKeyboardShortcuts());

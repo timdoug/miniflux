87f9e3564eedac3c4afa1ee60a2519802e695a41
Tim Douglas <me@timdoug.com>
2019-08-04 21:17:57 +0000

    Do not allow scrolling past first/last item

--- ui/static/js/app.js
+++ ui/static/js/app.js
@@ -437,7 +437,7 @@ function goToPreviousListItem() {
         return;
     }
 
-    for (let i = 0; i < items.length; i++) {
+    for (let i = 1; i < items.length; i++) {
         if (items[i].classList.contains("current-item")) {
             items[i].classList.remove("current-item");
 
@@ -469,7 +469,7 @@ function goToNextListItem() {
         return;
     }
 
-    for (let i = 0; i < items.length; i++) {
+    for (let i = 0; i < items.length - 1; i++) {
         if (items[i].classList.contains("current-item")) {
             items[i].classList.remove("current-item");
 

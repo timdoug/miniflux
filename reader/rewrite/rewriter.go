9266bdfa58a13ac6033582d1141928ff70ca6943
Tim Douglas <me@timdoug.com>
2019-08-05 15:51:20 +0000

    Do not mangle PDF links

--- reader/rewrite/rewriter.go
+++ reader/rewrite/rewriter.go
@@ -22,7 +22,6 @@ func Rewriter(entryURL, entryContent, customRewriteRules string) string {
 	}
 
 	rules := strings.Split(rulesList, ",")
-	rules = append(rules, "add_pdf_download_link")
 
 	logger.Debug(`[Rewrite] Applying rules %v for %q`, rules, entryURL)
 
@@ -37,12 +36,6 @@ func Rewriter(entryURL, entryContent, customRewriteRules string) string {
 			entryContent = addDynamicImage(entryURL, entryContent)
 		case "add_youtube_video":
 			entryContent = addYoutubeVideo(entryURL, entryContent)
-		case "add_invidious_video":
-			entryContent = addInvidiousVideo(entryURL, entryContent)
-		case "add_youtube_video_using_invidious_player":
-			entryContent = addYoutubeVideoUsingInvidiousPlayer(entryURL, entryContent)
-		case "add_pdf_download_link":
-			entryContent = addPDFLink(entryURL, entryContent)
 		case "nl2br":
 			entryContent = replaceLineFeeds(entryContent)
 		case "convert_text_link", "convert_text_links":

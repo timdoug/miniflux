11700b56171df37ad3b00e1f72071dd533f01351
Tim Douglas <me@timdoug.com>
2022-04-06 15:33:43 +0000

    Mark items as read upon selection

--- ui/static/js/app.js
+++ ui/static/js/app.js
@@ -464,6 +464,7 @@ function goToListItem(offset) {
     if (document.querySelector(".current-item") === null) {
         items[0].classList.add("current-item");
         items[0].querySelector('.item-header a').focus();
+        handleEntryStatus((offset > 0) ? "next" : "previous", items[0], true);
         return;
     }
 
@@ -478,6 +479,7 @@ function goToListItem(offset) {
             item.classList.add("current-item");
             DomHelper.scrollPageTo(item);
             item.querySelector('.item-header a').focus();
+            handleEntryStatus((offset > 0) ? "next" : "previous", item, true);
 
             break;
         }

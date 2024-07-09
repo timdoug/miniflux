8972878bf4bc15767a4cd1cf536019084c263e60
Tim Douglas <me@timdoug.com>
2024-07-09 20:34:44 +0000

    Add full content to each item

--- internal/ui/starred_entries.go
+++ internal/ui/starred_entries.go
@@ -25,7 +25,6 @@ func (h *handler) showStarredPage(w http.ResponseWriter, r *http.Request) {
 	builder.WithSorting("id", user.EntryDirection)
 	builder.WithOffset(offset)
 	builder.WithLimit(user.EntriesPerPage)
-	builder.WithoutContent()
 
 	entries, count, err := builder.GetEntriesWithCount()
 	if err != nil {

831cb64274aadeb3eeaf6fabda3dbd0d78b25a09
Tim Douglas <me@timdoug.com>
2024-07-09 20:28:06 +0000

    Fix key bindings

--- internal/ui/static/js/app.js
+++ internal/ui/static/js/app.js
@@ -1205,12 +1205,12 @@ function initializeKeyboardShortcuts() {
     keyboardHandler.on("C", () => openCommentLinkAction(true));
 
     // Entry management
-    keyboardHandler.on("m", () => handleEntryStatus("next"));
-    keyboardHandler.on("M", () => handleEntryStatus("previous"));
+    keyboardHandler.on("u", () => handleEntryStatus("next"));
+    keyboardHandler.on("U", () => handleEntryStatus("previous"));
     keyboardHandler.on("A", markPageAsReadAction);
-    keyboardHandler.on("s", () => handleSaveEntryAction());
+    keyboardHandler.on("S", () => handleSaveEntryAction());
     keyboardHandler.on("d", handleFetchOriginalContentAction);
-    keyboardHandler.on("f", () => handleStarAction());
+    keyboardHandler.on("s", () => handleStarAction());
 
     // Feed actions
     keyboardHandler.on("F", goToFeedPage);

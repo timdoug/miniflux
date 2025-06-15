529f696e00674df61de1d7fd0ff0dd013a60791c
Tim Douglas <me@timdoug.com>
2025-06-15 16:21:40 +0000

    Always scroll selected item to top of window

--- internal/ui/static/js/app.js
+++ internal/ui/static/js/app.js
@@ -29,14 +29,7 @@ function getVisibleElements(selector) {
  * @param {boolean} evenIfOnScreen
  */
 function scrollPageTo(element, evenIfOnScreen) {
-    const windowScrollPosition = window.scrollY;
-    const windowHeight = document.documentElement.clientHeight;
-    const viewportPosition = windowScrollPosition + windowHeight;
-    const itemBottomPosition = element.offsetTop + element.offsetHeight;
-
-    if (evenIfOnScreen || viewportPosition - itemBottomPosition < 0 || viewportPosition - element.offsetTop > windowHeight) {
-        window.scrollTo(0, element.offsetTop - 10);
-    }
+    window.scrollTo(0, element.offsetTop - 10);
 }
 
 // OnClick attaches a listener to the elements that match the selector.

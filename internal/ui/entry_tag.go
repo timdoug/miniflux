9056e28a596593fff3b9cd5ff07df4b902da434c
Tim Douglas <me@timdoug.com>
2025-08-08 14:32:54 -0400

    Add starred counts to title bar

--- internal/ui/entry_tag.go
+++ internal/ui/entry_tag.go
@@ -83,6 +83,7 @@ func (h *handler) showTagEntryPage(w http.ResponseWriter, r *http.Request) {
 	view.Set("prevEntryRoute", prevEntryRoute)
 	view.Set("user", user)
 	view.Set("countUnread", h.store.CountUnreadEntries(user.ID))
+	view.Set("countStarred", h.store.CountStarredEntries(user.ID))
 	view.Set("countErrorFeeds", h.store.CountUserFeedsWithErrors(user.ID))
 	view.Set("hasSaveEntry", h.store.HasSaveEntry(user.ID))
 

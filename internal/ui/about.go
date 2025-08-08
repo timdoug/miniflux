99e64bf993b226366b071719787adc222d5f810e
Tim Douglas <me@timdoug.com>
2025-08-08 14:32:54 -0400

    Add starred counts to title bar

--- internal/ui/about.go
+++ internal/ui/about.go
@@ -32,6 +32,7 @@ func (h *handler) showAboutPage(w http.ResponseWriter, r *http.Request) {
 	view.Set("menu", "settings")
 	view.Set("user", user)
 	view.Set("countUnread", h.store.CountUnreadEntries(user.ID))
+	view.Set("countStarred", h.store.CountStarredEntries(user.ID))
 	view.Set("countErrorFeeds", h.store.CountUserFeedsWithErrors(user.ID))
 	view.Set("globalConfigOptions", config.Opts.SortedOptions(true))
 	view.Set("postgres_version", h.store.DatabaseVersion())

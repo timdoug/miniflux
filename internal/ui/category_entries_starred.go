7af7012fae4cecab1e67c1e513bbfd44a1a0afe4
Tim Douglas <me@timdoug.com>
2025-08-08 14:32:54 -0400

    Add starred counts to title bar

--- internal/ui/category_entries_starred.go
+++ internal/ui/category_entries_starred.go
@@ -64,6 +64,7 @@ func (h *handler) showCategoryEntriesStarredPage(w http.ResponseWriter, r *http.
 	view.Set("menu", "categories")
 	view.Set("user", user)
 	view.Set("countUnread", h.store.CountUnreadEntries(user.ID))
+	view.Set("countStarred", h.store.CountStarredEntries(user.ID))
 	view.Set("countErrorFeeds", h.store.CountUserFeedsWithErrors(user.ID))
 	view.Set("hasSaveEntry", h.store.HasSaveEntry(user.ID))
 	view.Set("showOnlyStarredEntries", true)

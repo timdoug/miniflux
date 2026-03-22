55542ed11b0c9b0b729021e07684869d58ad9b95
Tim Douglas <me@timdoug.com>
2026-03-22 14:07:34 -0400

    Add starred counts to title bar

--- internal/ui/category_create.go
+++ internal/ui/category_create.go
@@ -23,6 +23,7 @@ func (h *handler) showCreateCategoryPage(w http.ResponseWriter, r *http.Request)
 	view.Set("user", user)
 	navMetadata, _ := h.store.GetNavMetadata(user.ID)
 	view.Set("countUnread", navMetadata.CountUnread)
+	view.Set("countStarred", navMetadata.CountStarred)
 	view.Set("countErrorFeeds", navMetadata.CountErrorFeeds)
 
 	response.HTML(w, r, view.Render("create_category"))

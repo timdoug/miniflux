d5764706218cf9e000c73603637b76cf3444f9e4
Tim Douglas <me@timdoug.com>
2026-03-22 14:07:34 -0400

    Add starred counts to title bar

--- internal/ui/api_key_save.go
+++ internal/ui/api_key_save.go
@@ -32,6 +32,7 @@ func (h *handler) saveAPIKey(w http.ResponseWriter, r *http.Request) {
 		view.Set("menu", "settings")
 		view.Set("user", user)
 		view.Set("countUnread", h.store.CountUnreadEntries(user.ID))
+		view.Set("countStarred", h.store.CountStarredEntries(user.ID))
 		view.Set("countErrorFeeds", h.store.CountUserFeedsWithErrors(user.ID))
 		view.Set("errorMessage", validationErr.Translate(user.Language))
 		response.HTML(w, r, view.Render("create_api_key"))

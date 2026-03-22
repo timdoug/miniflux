46a2abbb76b6c19b87d4178721dba9a296820d8d
Tim Douglas <me@timdoug.com>
2026-03-22 14:07:34 -0400

    Add starred counts to title bar

--- internal/ui/user_edit.go
+++ internal/ui/user_edit.go
@@ -49,6 +49,7 @@ func (h *handler) showEditUserPage(w http.ResponseWriter, r *http.Request) {
 	view.Set("user", user)
 	navMetadata, _ := h.store.GetNavMetadata(user.ID)
 	view.Set("countUnread", navMetadata.CountUnread)
+	view.Set("countStarred", navMetadata.CountStarred)
 	view.Set("countErrorFeeds", navMetadata.CountErrorFeeds)
 
 	response.HTML(w, r, view.Render("edit_user"))
